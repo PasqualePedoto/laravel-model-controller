3204e410e4f5272e19b9cc36a21ac99c1c860a38
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-19 15:20:47 +0200

    Added all file that we need

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    {{-- TITLE --}}
+    <title>{{ env('APP_NAME') }}</title>
+
+    {{-- STYLE --}}
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+    
 </body>
 </html>
